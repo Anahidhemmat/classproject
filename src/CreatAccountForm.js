2be3b925325d1eaca3d7b4af62881eb7e721aadf
Anahid <anahidhemmat2130@gmail.com>
2022-05-03 19:38:27 +0400

    sending data to server

--- src/CreatAccountForm.js
+++ src/CreatAccountForm.js
@@ -5,26 +5,53 @@ function CreateAccountForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleUsernameChange(e) {
-    setUsername(e.target.value);
+  //   function handleUsernameChange(e) {
+  //     setUsername(e.target.value);
+  //   }
+
+  //   function handleEmailChange(e) {
+  //     setEmail(e.target.value);
+  //   }
+
+  //   function handlePasswordChange(e) {
+  //     setPassword(e.target.value);
+  //   }
+
+  function handleChange(e) {
+    if (e.target.name === "username") {
+      setUsername(e.target.value);
+    } else if (e.target.name === "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name === "password") {
+      setPassword(e.target.value);
+    }
   }
 
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  function handlePasswordChange(e) {
-    setPassword(e.target.value);
-  }
+    console.log("sending data to server");
 
+    fetch("https://httpstat.us/200", {
+      method: "POStT",
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <input
           type="text"
           name="username"
           value={username}
-          onChange={handleUsernameChange}
+          onChange={handleChange}
           placeholder="Username"
         />
       </div>
@@ -33,7 +60,7 @@ function CreateAccountForm() {
           type="text"
           name="email"
           value={email}
-          onChange={handleEmailChange}
+          onChange={handleChange}
           placeholder="Email"
         />
       </div>
@@ -42,10 +69,11 @@ function CreateAccountForm() {
           type="text"
           name="password"
           value={password}
-          onChange={handlePasswordChange}
+          onChange={handleChange}
           placeholder="Password"
         />
       </div>
+      <button>submit</button>
     </form>
   );
 }
